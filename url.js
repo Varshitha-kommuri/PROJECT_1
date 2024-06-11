34e54339665cc7dca6026e7717ffd049018e4be4
Varshitha-kommuri <varshithakommuri04@gmail.com>
2024-06-11 14:23:57 +0530

    Updated url

--- url.js
+++ url.js
@@ -1,2 +1,2 @@
 //module.exports = `mongodb://localhost:27017`
-module.exports = 'mongodb+srv://admin:<password>@cluster0.1bsk973.mongodb.net/'
+module.exports = 'mongodb+srv://admin:admin@cluster0.1bsk973.mongodb.net/'
